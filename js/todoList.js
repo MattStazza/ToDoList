0fc85276d8a920dac7d5c531cb90a6e199eb46e1
MattStazza <matthewstazza@yahoo.com.au>
2020-09-09 16:38:35 +0800

    Adding a ToDo

--- js/todoList.js
+++ js/todoList.js
@@ -7,9 +7,9 @@ const input = document.getElementById("input");
 
 const test = "test";
 //Classes names
-const CHECK = "checked_btn";
-const UNCHECK = "unchecked_btn";
-const LINE_THROUGH = "line-through";
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
 
 //Show todays date
 const options = { weekday: "long", month: "short", day: "numeric" };
@@ -18,16 +18,38 @@ const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 //Add to do fucntion
-function addToDo(toDo) {
+function addToDo(toDo, id, done, trash) {
+  if (trash) {
+    return;
+  }
+  const DONE = done ? CHECK : UNCHECK;
+  const LINE = done ? LINE_THROUGH : "";
+
   const item = `
                 <li class="item">
-                    <input type="checkbox" />
-                    <p class="text">${toDo}</p>
-                    <i class="delete_btn fa fa-trash-o de" job="delete"></i>
+                    <i class="fa ${DONE} co" job="complete" id="${id}"</i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
                 </li>
                 `;
   const position = "beforeend";
   list.insertAdjacentHTML(position, item);
 }
 
-addToDo("drink coffee boi");
+//Adding a ToDo by clicking [ENTER].
+//Will add a new item.
+//Wont add an item if the input is empty.
+//The input is reset.
+document.addEventListener("keyup", function (event) {
+  if (event.keyCode == 13) {
+    const toDo = input.value;
+
+    //if the input isn't empty
+    if (toDo) {
+      addToDo(toDo);
+    }
+    input.value = "";
+  }
+});
+
+addToDo("test", 1, false, false);
